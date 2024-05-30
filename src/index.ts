157fb31dee3f7e333bde8a757864234ba37e5934
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2024-05-30 22:38:11 +0800

    fix: may overwrite .js, cjs and other build output
    fix: may overwrite .js, cjs and other build output
    
    squash: lint & change var name
    Co-Authored-By: Shigma <shigma10826@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,8 @@ declare module 'tsconfig-utils' {
 }
 
 export interface Config {
-  loaders?: string[]
+  ignored?: string[]
+  converters?: string[]
 }
 
 export async function build(cwd: string, args: string[] = []) {
@@ -22,7 +23,8 @@ export async function build(cwd: string, args: string[] = []) {
   const emitDeclarationOnly = config.get('emitDeclarationOnly')
   if (!outDir || !rootDir || noEmit || emitDeclarationOnly) return
 
-  const loaders = config.atsc?.loaders || ['.yaml', '.yml']
+  const converters = config.atsc?.converters || ['.yaml', '.yml']
+  const ignored = config.atsc?.ignored || ['.ts', '.mts', '.cts', '.tsx', '.mjs', '.cjs', '.js', '.jsx']
 
   const files = await globby(['**'], {
     cwd: resolve(cwd, rootDir),
@@ -31,15 +33,16 @@ export async function build(cwd: string, args: string[] = []) {
 
   await Promise.all(files.map(async (file) => {
     const ext = extname(file)
-    if (['.ts', '.mts', '.cts', '.tsx'].includes(ext)) return
+    if (ignored.includes(ext)) return
+
     const src = resolve(cwd, rootDir, file)
     const dest = resolve(cwd, outDir, file)
     await fs.mkdir(dirname(dest), { recursive: true })
-    if (!loaders.includes(ext)) {
-      await fs.copyFile(src, dest)
-    } else if (['.yaml', '.yml'].includes(ext)) {
+    if (converters.includes(ext) && ['.yml', '.yaml'].includes(ext)) {
       const data = yaml.load(await fs.readFile(src, 'utf8'))
       await fs.writeFile(dest.slice(0, -ext.length) + '.json', JSON.stringify(data) || '')
+    } else {
+      await fs.copyFile(src, dest)
     }
   }))
 }
